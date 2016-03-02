35bc2600be0da411f3a1859647612b30a5aa1b6d
tinmarino <tinmarino@gmail.com>
2016-03-02 19:17:42 +0100

    Frist screen

---
+++ core/src/com/aes/game/MainRenderer.java
@@ -0,0 +1,70 @@
+package com.aes.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+
+public class MainRenderer implements Screen
+ {
+
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void show() {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void render(float arg0) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+
+		batch.end();
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
--- core/src/com/aes/game/MyGdxMain.java
+++ core/src/com/aes/game/MyGdxMain.java
@@ -1,28 +1,19 @@
 package com.aes.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
-public class MyGdxMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class MyGdxMain extends Game {
+	Screen mainScreen; 
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		mainScreen = new MainRenderer(); 
+		setScreen(mainScreen);  
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-
-		batch.end();
+		super.render(); // Calling mainScreen.render()
 	}
 }
