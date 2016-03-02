947d26d6d14b0aead9109dfc0402c0983bdc24d9
tinmarino <tinmarino@gmail.com>
2016-03-02 20:02:36 +0100

    add table left and right, and debug table

--- core/src/com/aes/game/MainRenderer.java
+++ core/src/com/aes/game/MainRenderer.java
@@ -1,5 +1,6 @@
 package com.aes.game;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,6 +12,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 
@@ -24,6 +26,12 @@ public class MainRenderer implements Screen
 	Skin skin;
 	private TextButton myButton;
 	private Table table;
+	TextField textFieldClear; 
+	TextField textFieldCoded; 
+
+
+	Table leftTable; 
+	Table rightTable; 
 
 	@Override
 	public void show() {
@@ -50,21 +58,39 @@ public class MainRenderer implements Screen
     			}
 		);
 
+		// TextField1
+		textFieldClear = new TextField("This is a raw input ", skin);
+		textFieldCoded = new TextField("This will be the crippted text", skin);
+
     	
 		// TABLE 
     	table = new Table(); 
-    	table.setFillParent(true);
-    	
-    	table.add(myButton);
+		leftTable = new Table(); 
+		rightTable = new Table(); 
+		
+		leftTable.add(textFieldClear);
+		leftTable.row();
+		leftTable.add(textFieldCoded);
 
+    	rightTable.add(myButton);
+
+
+		
+		table.add(leftTable).expand(); 
+		table.add(rightTable).expand(); 
+    	table.setFillParent(true);
     	stage.addActor(table);
+		leftTable.debug(); 
+		rightTable.debug(); 
+		table.debug();
+
 	}
 
 
 	@Override
 	public void render(float delta) {
 		// CLEAR 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		// BATCH 
@@ -74,14 +100,15 @@ public class MainRenderer implements Screen
 
 		//STAGE 
 		stage.act(delta); 
-
 		stage.draw(); 
+		//Table.drawDebug( stage);
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
 		stage.dispose(); 
+		skin.dispose();
 
 	}
 
