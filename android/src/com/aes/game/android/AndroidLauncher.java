08a63c2d43f0aebc6d249daa27958147c36a5aea
tinmarino <tinmarino@gmail.com>
2016-03-02 19:05:40 +0100

    first commit

---
+++ android/src/com/aes/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aes.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aes.game.MyGdxMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxMain(), config);
+	}
+}
---
+++ desktop/src/com/aes/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aes.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aes.game.MyGdxMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxMain(), config);
+	}
+}
