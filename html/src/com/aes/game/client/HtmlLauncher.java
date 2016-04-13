15e9a2dd06e187fe7c4df1bcd7a5dbe6bf835649
tinmarino <tinmarino@gmail.com>
2016-04-13 14:14:22 +0200

    Before resizing well

--- html/src/com/aes/game/client/HtmlLauncher.java
+++ html/src/com/aes/game/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.aes.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.GwtClipboard;
 import com.aes.game.Global;
 import com.aes.game.MyAesGame;
 
@@ -12,6 +13,7 @@ public class HtmlLauncher extends GwtApplication {
         public GwtApplicationConfiguration getConfig () {
                 //return new GwtApplicationConfiguration(320*2, 480*2);
 				Global.platformOs = new PlatformHtml();
+				Global.clipboard  = new GwtClipboard();
                 return new GwtApplicationConfiguration((int)(320 * 1.5f), (int)(480 * 1.5f));
         }
 
